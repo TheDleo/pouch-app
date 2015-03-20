7b533f921ea2b3a65461a74f37d779c803c1e8d4
Dan Martin <dmartin@fh.org>
2015-03-19 17:24:19 -0700

    first working version

--- www/js/app.js
+++ www/js/app.js
@@ -9,48 +9,74 @@ var localDB = new PouchDB("todos");
 var remoteDB = new PouchDB("http://130.211.130.95:5984/todos");
 
 pouchApp.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-    
-    localDB.sync(remoteDB, { live : true });
-    
-  });
+	$ionicPlatform.ready(function() {
+	    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+	    // for form inputs)
+	    if(window.cordova && window.cordova.plugins.Keyboard) {
+	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	    }
+	    if(window.StatusBar) {
+	      StatusBar.styleDefault();
+	    }
+	    localDB.sync(remoteDB, { live : true });
+	});
 });
 
 pouchApp.controller("PouchController", function($scope, PouchDBListener, $ionicPopup) {
 
 	$scope.todos = [];
 
-});
+	$scope.create = function() {
+		$ionicPopup.prompt({
+			title: "Enter a new TODO item",
+			inputType: "text"
+		}).then(function(result) {
+			if(result !== "") {
+				if ($scope.hasOwnProperty("todos") !== true) {
+					$scope.todos = [];
+				};
+				localDB.post({title: result});
+			}
+		});
+	}
 
+    $scope.$on("add", function(event, todo) {
+    	$scope.todos.push(todo);
+    });
+
+    $scope.$on("delete", function(event, id) {
+    	for (var i = 0; i < $scope.todos; i++) {
+    		if($scope.todos[i]._id === id) {
+    			$scope.todos.splice(i, 1);
+    		}
+    	}
+    });
+});
 
 pouchApp.factory("PouchDBListener", ["$rootScope", function($rootScope) {
 
-	localDB.changes([
+	localDB.changes({
 		continuous: true,
 		onChange: function(change) {
-			$rootScope.$apply(function() {
-				localDB.get(change.id, function(err, doc) {
-					$rootScope.$apply(function() {
-						if(err) {
-							console.error(err);
-						}
-						$rootScope.$broadcast("add", doc);
+			if(!change.deleted) {
+				$rootScope.$apply(function() {
+					localDB.get(change.id, function(err, doc) {
+						$rootScope.$apply(function() {
+							if (err) {
+								console.error(err);
+							}
+							$rootScope.$broadcast("add", doc);
+						});
 					});
 				});
-			});
-		} else {
-			$rootScope.$apply(function() {
-				$rootScope.$broadcast("delete", change.id);
-			});
+			} else {
+				$rootScope.$apply(function() {
+					$rootScope.$broadcast("delete", change.id);
+				});
+			}
 		}
-	]);
+	});
+
+	return true;
 
 }]);
